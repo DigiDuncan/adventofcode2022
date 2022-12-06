33ad5475ad0a5d1be77398f8e6e1a3b5e1cbf226
DigiDuncan <digiduncan@gmail.com>
2022-12-06 12:34:09 -0500

    make a template day

---
+++ adventofcode2022/days/template.py
@@ -0,0 +1,13 @@
+from adventofcode2022.lib.aoc import DayTemplate
+
+
+class Day(DayTemplate):
+    def __init__(self):
+        super().__init__(0)
+
+    def part_1(self):
+        super().part_1()
+
+
+    def part_2(self):
+        super().part_2()
